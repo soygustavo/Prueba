839bcf19bfcab50a788a3fad96cff745ae6f4076
soygustavo <gjsuarez@gmail.com>
2012-06-13 02:38:03 -0300

    cambie de rama
    en esta rama hay modificaciones drasticas

--- GlucoParental/gen/com/mi/proyectoFinal/R.java
+++ GlucoParental/gen/com/mi/proyectoFinal/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package mi.prueba.github;
+package com.mi.proyectoFinal;
 
 public final class R {
     public static final class attr {
@@ -14,10 +14,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050002;
-        public static final int button2=0x7f050003;
-        public static final int checkBox1=0x7f050001;
-        public static final int textView1=0x7f050000;
+        public static final int imageButton1=0x7f050000;
+        public static final int imageButton2=0x7f050001;
+        public static final int textView1=0x7f050002;
+        public static final int textView2=0x7f050003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
---
+++ GlucoParental/src/com/mi/proyectoFinal/GlucoProActivity.java
@@ -0,0 +1,13 @@
+package com.mi.proyectoFinal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GlucoProActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
